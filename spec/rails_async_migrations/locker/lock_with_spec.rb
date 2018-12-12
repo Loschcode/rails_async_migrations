666d7a5fe4b075ef7322941800f88289a3a66906
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2018-12-12 18:52:46 -0300

    lock_with tested

---
+++ spec/rails_async_migrations/locker/lock_with_spec.rb
@@ -0,0 +1,48 @@
+RSpec.describe RailsAsyncMigrations::Locker::LockWith do
+  let(:resource_class) { FakeClass }
+  let(:resource_instance) { resource_class.new }
+  let(:method) { :free_method }
+  let(:instance) { described_class.new(resource_class, method) }
+
+  let(:change_to_it_passed) { resource_class.define_method(method, -> { 'it passed' }) }
+
+  before { change_to_it_passed }
+  subject { instance.perform }
+
+  context 'method not in the lockable list' do
+    it { is_expected.to be(false) }
+
+    context 'does not alter the method' do
+      before { subject }
+      it { expect(resource_instance.send(method)).to eq('it passed') }
+    end
+  end
+
+  context 'method in the lockable list' do
+    let(:method) { :change }
+    it { is_expected.to eq(true) }
+
+    context 'alter the method' do
+      before { subject }
+      it { expect(resource_instance.send(method)).not_to eq('it passed') }
+    end
+  end
+end
+
+ class FakeClass
+  def free_method
+    true
+  end
+
+  def change
+    'it passed'
+  end
+
+  def up
+    true
+  end
+
+  def down
+    true
+  end
+ end
