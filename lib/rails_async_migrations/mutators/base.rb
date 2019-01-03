94c29ce012a68501e2cc10f4ab2a7320f3c1d5cb
Laurent Schaffner <laurent.schaffner.code@gmail.com>
2019-01-03 16:25:19 -0300

    multiple async improved in queue

--- lib/rails_async_migrations/mutators/base.rb
+++ lib/rails_async_migrations/mutators/base.rb
@@ -4,14 +4,11 @@ module RailsAsyncMigrations
       # in some context we have to guess it from the file and current connection
       # for instance when we use class methods such as `turn_async`
       def migration_class
-        if instance.class == ActiveRecord::Migration
-          fetch_from_file
-        else
-          instance.class
-        end
+        fetch_from_file || instance.class
       end
 
       def fetch_from_file
+        return unless current_migration
         require current_migration.filename
         current_migration.name.constantize
       end
